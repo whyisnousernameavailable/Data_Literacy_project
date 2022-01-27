50880a5d0a393006d0e56648b402cce77345b8dc
whyisnousernameavailable <jonathan.reitemann@web.de>
2022-01-27 19:48:09 +0100

    Upload requested

--- Python_Code/Test1.py
+++ Python_Code/Test1.py
@@ -1,9 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
+from Python_Code.definitions import dtype_caracteristics
 
-test = np.array((8, 1, 10, -5, -4 , 7))
+test = np.array((8, 1, 10, -5, -4, 7))
 plt.plot(test)
 plt.show()
 
+data_characteristics = pd.read_csv('..//archive//caracteristics.csv',
+                                   encoding="ISO-8859-1", dtype=dtype_caracteristics)
+print(data_characteristics.head())
 
---
+++ Python_Code/definitions.py
@@ -0,0 +1,7 @@
+
+dtype_caracteristics = {'Num_Acc': int, 'jour': int, 'mois': int, 'an': int,
+                        'hrmn': int, 'lum': int, 'dep': int, 'com': int,
+                        'adr': str, 'gps': str, 'lat': int, 'long': int}
+
+
+
