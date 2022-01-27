a2b3fc30a1171226dc2fbdb88c86db73457d6e84
whyisnousernameavailable <jonathan.reitemann@web.de>
2022-01-27 21:53:08 +0100

    Added fixes to definitions

--- Python_Code/Test1.py
+++ Python_Code/Test1.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from Python_Code.definitions import dtype_caracteristics
+from Python_Code.definitions import dtype_caracteristics, dtype_places
 
 test = np.array((8, 1, 10, -5, -4, 7))
 plt.plot(test)
@@ -11,7 +11,10 @@ plt.show()
 data_characteristics = pd.read_csv('..//archive//caracteristics.csv',
                                    encoding="ISO-8859-1", dtype=dtype_caracteristics,
                                    on_bad_lines='warn', na_values=['-'])
-print(data_characteristics.head())
+data_places = pd.read_csv('..//archive//places.csv',
+                                   encoding="ISO-8859-1", dtype=dtype_places,
+                                   on_bad_lines='warn', na_values=['-'])
+print(data_places.head())
 
 data_characteristics['an'].value_counts().plot(kind='bar')
 plt.show()
--- Python_Code/definitions.py
+++ Python_Code/definitions.py
@@ -6,9 +6,9 @@ dtype_caracteristics = {'Num_Acc': pd.Int64Dtype(), 'jour': pd.Int8Dtype(), 'moi
                         'adr': str, 'gps': str, 'lat': pd.Float64Dtype(), 'long': pd.Float64Dtype(),
                         'atm': pd.Int8Dtype(), 'agg': pd.Int8Dtype(), 'int': pd.Int8Dtype()}
 
-dtype_places = {'Num_Acc': pd.Int64Dtype(), 'catr': pd.Int8Dtype(), 'voie': pd.Int32Dtype(),
+dtype_places = {'Num_Acc': pd.Int64Dtype(), 'catr': pd.Int8Dtype(), 'voie': str,
                 'v1': pd.Int8Dtype(), 'v2': str, 'circ': pd.Int8Dtype(),
                 'nbv': pd.Int8Dtype(), 'vosp': pd.Int8Dtype(), 'prof': pd.Int8Dtype(),
-                'pr': pd.Int16Dtype(), 'pr1': pd.Int32Dtype(), 'plan': pd.Int8Dtype(), 'lartpc': pd.Int32Dtype(),
+                'pr': str, 'pr1': str, 'plan': pd.Int8Dtype(), 'lartpc': pd.Int32Dtype(),
                 'larrout': pd.Int32Dtype(), 'surf': pd.Int8Dtype(), 'infra': pd.Int8Dtype(),
                 'situ': pd.Int8Dtype(), 'env1': pd.Int32Dtype()}
