fb22212298798b3aed77a07ec2981a9895471dfc
whyisnousernameavailable <jonathan.reitemann@web.de>
2022-01-27 23:22:59 +0100

    Added remaining datatypes and updated test1 file

--- Python_Code/Test1.py
+++ Python_Code/Test1.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from Python_Code.definitions import dtype_caracteristics, dtype_places
+from Python_Code.definitions import dtype_caracteristics, dtype_places, dtype_vehicles, dtype_users, dtype_holidays
 
 test = np.array((8, 1, 10, -5, -4, 7))
 plt.plot(test)
@@ -14,7 +14,21 @@ data_characteristics = pd.read_csv('..//archive//caracteristics.csv',
 data_places = pd.read_csv('..//archive//places.csv',
                                    encoding="ISO-8859-1", dtype=dtype_places,
                                    on_bad_lines='warn', na_values=['-'])
+data_holidays = pd.read_csv('..//archive//holidays.csv',
+                                   encoding="ISO-8859-1", dtype=dtype_holidays,
+                                   on_bad_lines='warn', na_values=['-'], parse_dates=['ds'])
+data_users = pd.read_csv('..//archive//users.csv',
+                                   encoding="ISO-8859-1", dtype=dtype_users,
+                                   on_bad_lines='warn', na_values=['-'])
+data_vehicles = pd.read_csv('..//archive//vehicles.csv',
+                                   encoding="ISO-8859-1", dtype=dtype_vehicles,
+                                   on_bad_lines='warn', na_values=['-'])
 print(data_places.head())
+print(data_characteristics.head())
+print(data_users.head())
+print(data_vehicles.head())
+print(data_holidays.head())
+
 
 data_characteristics['an'].value_counts().plot(kind='bar')
 plt.show()
--- Python_Code/definitions.py
+++ Python_Code/definitions.py
@@ -12,3 +12,13 @@ dtype_places = {'Num_Acc': pd.Int64Dtype(), 'catr': pd.Int8Dtype(), 'voie': str,
                 'pr': str, 'pr1': str, 'plan': pd.Int8Dtype(), 'lartpc': pd.Int32Dtype(),
                 'larrout': pd.Int32Dtype(), 'surf': pd.Int8Dtype(), 'infra': pd.Int8Dtype(),
                 'situ': pd.Int8Dtype(), 'env1': pd.Int32Dtype()}
+
+dtype_users = {'Num_Acc': pd.Int64Dtype(), 'place': pd.Int8Dtype(), 'catu': pd.Int8Dtype(),
+               'grav': pd.Int8Dtype(), 'sexe': pd.Int8Dtype(), 'trajet': pd.Int8Dtype(),
+               'secu': pd.Int8Dtype(), 'locp': pd.Int8Dtype(), 'actp': pd.Int8Dtype(),
+               'etatp': pd.Int8Dtype(), 'an_nais': pd.Int16Dtype(), 'num_veh': str}
+
+dtype_vehicles = {'Num_Acc': pd.Int64Dtype(), 'catv': pd.Int8Dtype(), 'num_veh': str}
+
+dtype_holidays = {'ds': str, 'holiday': str}
+
