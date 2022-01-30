2efef5e2d939eb47cf0c017d9e974bfec6dbfec7
whyisnousernameavailable <jonathan.reitemann@web.de>
2022-01-30 19:36:40 +0100

    PlotExample

---
+++ Python_Code/Test3.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+speed = [0.1, 17.5, 40, 48, 52, 69, 88]
+lifespan = [2, 8, 70, 1.5, 25, 12, 28]
+index = ['snail', 'pig', 'elephant',
+         'rabbit', 'giraffe', 'coyote', 'horse']
+df = pd.DataFrame({'speed': speed,
+                   'lifespan': lifespan}, index=index)
+ax = df.plot.bar(rot=0, stacked=True)
+plt.show()
+
