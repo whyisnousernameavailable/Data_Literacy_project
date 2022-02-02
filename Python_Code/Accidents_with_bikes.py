8a13512303e71428d1f0955f65b567a2f147be51
whyisnousernameavailable <jonathan.reitemann@web.de>
2022-02-02 16:17:40 +0100

    Added accidents with bikes table calculation etc stuff§

---
+++ Python_Code/Accidents_with_bikes.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from tueplots import bundles
+
+from Python_Code.definitions import load_users_csv, load_caracteristics_csv, load_vehicles_csv, load_places_csv
+
+plt.rcParams.update(bundles.neurips2021(usetex=False))
+
+data_users = load_users_csv()
+data_caracteristics = load_caracteristics_csv()
+data_vehicles = load_vehicles_csv()
+data_places = load_places_csv()
+
+user_acc_data = pd.merge(data_users[['Num_Acc', 'grav', 'catu']],
+                            data_caracteristics[['Num_Acc', 'an']],
+                            how="left",
+                            on=['Num_Acc', 'Num_Acc'])
+
+user_acc_catv_data = pd.merge(user_acc_data,
+                              data_vehicles[['Num_Acc', 'catv']],
+                              how="left",
+                              on=['Num_Acc', 'Num_Acc'])
+user_acc_catv_places_data = pd.merge(user_acc_catv_data,
+                                     data_places[['Num_Acc', 'vosp', 'prof']],
+                                     how="left",
+                                     on=['Num_Acc', 'Num_Acc'])
+
+
+if isinstance(user_acc_catv_places_data, pd.DataFrame):
+    print('I am a dataframe')
+
+data_bike_accidents = user_acc_catv_places_data.loc[user_acc_catv_places_data['catv'] == 1]
+
+print(data_bike_accidents[['vosp']].value_counts())
